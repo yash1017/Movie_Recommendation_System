fe483a18f2ff22f9c8bebcdaf31535db6855e81b
Thokala Yaswanth babu <103556819+yash1017@users.noreply.github.com>
2026-05-05 14:29:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -151,5 +151,9 @@ def recommend():
         vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
         movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
